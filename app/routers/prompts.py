9a929219844fb14f6be2e05f717273262acd0dbb
Thomas Chardo <chardonnens.thomas@protonmail.com>
2023-06-13 03:43:41 +0200

    (fix): feed fixed

--- app/routers/prompts.py
+++ app/routers/prompts.py
@@ -89,23 +89,21 @@ async def get_prompt_contents(
     return prompts.get_prompt_contents(prompt_id, db)
 
 
-@router.get(
-    "/prompts/feed", response_model=list[PromptSubjectAndContents], tags=["prompts"]
-)
+@router.get("/feed", response_model=list[PromptSubjectAndContents], tags=["prompts"])
 async def get_feed(
     db: Session = Depends(get_db),
     current_user: User = Depends(get_current_user),
 ):
     logging.info("Getting feed")
-    # list_of_followings = followings.get_followings_by_user_id(current_user.id, db)
-    # feed = list[PromptSubjectAndContents]
-    # for following in list_of_followings:
-    #     list_of_prompts = prompts.get_last_three_prompts_by_user_id(
-    #         following.following_id, db
-    #     )
-    #     logging.info(list_of_prompts)
-    #     for prompt in list_of_prompts:
-    #         logging.info(prompt)
-    #         prompt_subject_and_contents = prompts.get_prompt_contents(prompt.id, db)
-    #         feed.append(prompt_subject_and_contents)
-    return []
+    list_of_followings = followings.get_followings_by_user_id(current_user.id, db)
+    feed = []  # Correctly initialize an empty list
+    for following in list_of_followings:
+        list_of_prompts = prompts.get_last_three_prompts_by_user_id(
+            following.following_id, db
+        )
+        logging.info(list_of_prompts)
+        for prompt in list_of_prompts:
+            logging.info(prompt)
+            prompt_subject_and_contents = prompts.get_prompt_contents(prompt.id, db)
+            feed.append(prompt_subject_and_contents)
+    return feed  # Return the feed list
