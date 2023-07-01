2d79d8c964f7eabcb6717d322b440ae6bfaae558
Thomas Chardo <chardonnens.thomas@protonmail.com>
2023-07-01 15:10:13 +0200

    (feat): update input schema on curious endpoint

--- app/routers/contents.py
+++ app/routers/contents.py
@@ -1,6 +1,7 @@
 import json
 import logging
 import os
+from pydantic import BaseModel
 import redis
 
 from fastapi import APIRouter, HTTPException
@@ -43,22 +44,27 @@ async def startup_event():
     cache = redis.Redis(connection_pool=pool)
 
 
+class CuriousInput(BaseModel):
+    prompt: str
+    is_private: bool
+
+
 @router.post(
     "/chat",
     response_description="ChatGPT response",
     tags=["contents"],
 )
-async def chat(request: AIPrompt, current_user: User = Depends(get_current_user)):
+async def chat(request: CuriousInput, current_user: User = Depends(get_current_user)):
     if cache.get(request.prompt):
         logging.info("Cache hit")
-        response: str = cache.get(request.prompt)
+        response: str = cache.get(request)
         json_dict = json.loads(response)
         ai_response = AIResponse.parse_obj(json_dict)
     else:
         logging.info("Cache miss")
-        ai_response: AIResponse = gpt_json_response(request.prompt)
+        ai_response: AIResponse = gpt_json_response(request)
         json_string = ai_response.json()
-        cache.set(request.prompt, str(json_string))
+        cache.set(request, str(json_string))
 
     if (
         ai_response.basic_subjects[0].detailed_name
@@ -77,49 +83,46 @@ async def chat(request: AIPrompt, current_user: User = Depends(get_current_user)
     tags=["contents"],
 )
 async def curious(
-    request: AIPrompt,
+    request: CuriousInput,
     current_user: User = Depends(get_current_user),
     db: Session = Depends(get_db),
 ):
-    if cache.get(request.prompt):
+    cached_response = cache.get(request.prompt)
+
+    if cached_response:
         logging.info("Cache hit")
-        response: str = cache.get(request.prompt)
-        json_dict = json.loads(response)
+        json_dict = json.loads(cached_response)
         ai_response = AIResponse.parse_obj(json_dict)
     else:
         logging.info("Cache miss")
-        ai_response: AIResponse = gpt_json_response(request.prompt)
+        ai_response = gpt_json_response(request.prompt)
         json_string = ai_response.json()
-        cache.set(request.prompt, str(json_string))
+        cache.set(request.prompt, json_string)
 
-    all_prompt_subjects_and_contents: list[PromptSubjectAndContents] = []
+    all_prompt_subjects_and_contents = []
 
     async def process_subjects(
-        prompt: AIPrompt,
+        prompt: str,
         subjects: list[Subject],
         all_prompt_subjects_and_contents: list[PromptSubjectAndContents],
     ):
         for subject in subjects:
-            prompt_subject_and_contents: PromptSubjectAndContents = (
-                await AIResponseSubjectSearchEngines(
-                    prompt, subject.detailed_name, subject.description, db
-                )
+            prompt_subject_and_contents = await AIResponseSubjectSearchEngines(
+                prompt, subject.detailed_name, db
             )
             all_prompt_subjects_and_contents.append(prompt_subject_and_contents)
         return all_prompt_subjects_and_contents
 
-    created_prompt: Prompt = prompts.create_prompt(
-        (
-            PromptCreate(
-                title=str(request.prompt),
-                keywords=ai_response.main_subject_of_the_prompt,
-                is_private=request.is_private,
-                user_id=current_user.id,
-            )
+    created_prompt = prompts.create_prompt(
+        PromptCreate(
+            title=request.prompt,
+            keywords=ai_response.main_subject_of_the_prompt,
+            is_private=request.is_private,
+            user_id=current_user.id,
         ),
         db,
     )
-    all_prompt_subjects_and_contents = []
+
     if ai_response.basic_subjects != "string":
         all_prompt_subjects_and_contents = await process_subjects(
             created_prompt,
@@ -130,6 +133,7 @@ async def curious(
         raise HTTPException(
             status_code=404, detail="No basic subjects found, LLM failed"
         )
+
     if ai_response.deeper_subjects != "string":
         all_prompt_subjects_and_contents = await process_subjects(
             created_prompt,
@@ -140,6 +144,7 @@ async def curious(
         raise HTTPException(
             status_code=404, detail="No deeper subjects found, LLM failed"
         )
+
     return all_prompt_subjects_and_contents
 
 
